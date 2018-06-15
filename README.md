98b4e7eaf15215c2d435cb78bf35b3348b739b20
Joe Lambert <joe@joelambert.co.uk>
2018-06-15 18:25:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Installer for a Lumberjack site with Bedrock
+
+## Usage
+
+Install this package globally via Composer:
+
+```
+composer global require rareloop/lumberjack-bedrock-installer
+```
+
+You can then scaffold out a new Lumberjack/Bedrock site using:
+
+```
+lumberjack-bedrock new my-site
+```
+
+The above command will create a folder in your current directory called `my-site` which contains the scaffolded out site.
---
+++ src/NewCommand.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Rareloop\Lumberjack\Installer;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class NewCommand extends Command
+{
+    protected $projectPath;
+    protected $escapedProjectPath;
+    protected $output;
+
+    protected function configure()
+    {
+        $this->setName('new');
+        $this->setDescription('Create a new Lumberjack project built on Bedrock');
+        $this->addArgument(
+            'name',
+            InputArgument::OPTIONAL,
+            'The name of the folder to create (defaults to `lumberjack-bedrock-site`)'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+
+        $projectFolderName = $input->getArgument('name') ?? 'lumberjack-bedrock-site';
+
+        $this->projectPath = getcwd().'/'.$projectFolderName;
+        $this->escapedProjectPath = escapeshellarg($this->projectPath);
+
+        if (file_exists($this->projectPath)) {
+            $output->writeln('<error>Can\'t install to: '.$this->projectPath.'. The directory already exists</error>');
+            return;
+        }
+
+        try {
+            $this->checkoutLatestBedrock();
+            $this->installComposerDependencies();
+            $this->checkoutLatestLumberjackTheme();
+            $this->addAdditionalDotEnvKeys();
+        } catch (\Exception $e) {
+            $output->writeln('<error>Install failed</error>');
+        }
+    }
+
+    protected function checkoutLatestBedrock()
+    {
+        $this->output->writeln('<info>Checking out Bedrock</info>');
+
+        $commands = [
+            'git clone --depth=1 git@github.com:roots/bedrock.git '.$this->escapedProjectPath,
+            'rm -rf '.$this->escapedProjectPath.'/.git',
+        ];
+
+        $process = new Process(implode(' && ', $commands));
+
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+    }
+
+    protected function installComposerDependencies()
+    {
+        $this->output->writeln('<info>Installing Composer Dependencies</info>');
+
+        $commands = [
+            'cd '.$this->escapedProjectPath,
+            'composer require '.implode(' ', $this->getComposerDependencies()),
+        ];
+
+        $process = new Process(implode(' && ', $commands));
+
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+    }
+
+    protected function getComposerDependencies()
+    {
+        return [
+            'rareloop/lumberjack-core',
+        ];
+    }
+
+    protected function checkoutLatestLumberjackTheme()
+    {
+        $this->output->writeln('<info>Adding Lumberjack theme</info>');
+
+        $themeDirectory = escapeshellarg($this->projectPath.'/web/app/themes/lumberjack');
+
+        $commands = [
+            'git clone --depth=1 git@github.com:rareloop/lumberjack.git '.$themeDirectory,
+            'rm -rf '.$themeDirectory.'/.git',
+        ];
+
+        $process = new Process(implode(' && ', $commands));
+
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+    }
+
+    protected function addAdditionalDotEnvKeys()
+    {
+        $this->output->writeln('<info>Updating .env.example</info>');
+
+        $file = fopen($this->projectPath.'/.env.example', 'a');
+
+        foreach ($this->getDotEnvLines() as $line) {
+            fwrite($file, $line);
+        }
+    }
+
+    protected function getDotEnvLines()
+    {
+        return [
+            "\n",
+            'APP_KEY=',
+        ];
+    }
+}
